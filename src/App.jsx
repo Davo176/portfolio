b098d63cc8a157cfcbc1ea2dcba22e059accc845
Davo176 <wil1234@live.com.au>
2023-09-13 22:02:09 +0930

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from './home';
+import About from './about';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/home/index.jsx
@@ -0,0 +1,14 @@
+function Home() {
+
+    return (
+      <div className="bg-slate-600 h-screen">
+        <h1 className="text-slate-100 text-9xl calsans">
+          <span>Will</span>
+          <br/>
+          <span>Davis</span>
+        </h1>
+      </div>
+    )
+}
+  
+export default Home
