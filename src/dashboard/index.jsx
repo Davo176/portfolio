cca804adfd2dda107b8b916f34e6a85a7fdb3423
Davo176 <wil1234@live.com.au>
2024-03-01 23:13:05 +1030

    optimisse dashboard for phone

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -3,8 +3,8 @@ import { TotalUsersGraph, TotalUsersFigure, LastUsedGraph } from "../components"
 function Dashboard() {
     return (
         <div className="p-2">
-            <div className="grid grid-cols-4 grid-rows-2 rounded-md border-2 border-teal-400 p-2">
-                <div className="col-span-3 row-span-2">
+            <div className="grid grid-cols-2 grid-rows-2 rounded-md border-2 border-teal-400 p-2 lg:grid-cols-4">
+                <div className="col-span-2 lg:col-span-3 lg:row-span-2">
                     <TotalUsersGraph />
                 </div>
                 <TotalUsersFigure />
